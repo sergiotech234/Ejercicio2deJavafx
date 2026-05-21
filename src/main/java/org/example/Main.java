fa8f71274045507684d16d689e3dc394b3114a9a
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2026-05-21 11:11:53 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,13 @@ import javafx.stage.Stage;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
+        //creacion del boton
         Button button1 = new Button("Haz Click aqui");
+        //accion que hace el boton anterior pero impreso en consola
         button1.setOnAction(e -> {
             System.out.println("Has pulsado");
         });
+        //Configuracion del programa
         Scene scene = new Scene(button1, 300, 200);
         primaryStage.setTitle("Aplicacion  de clic de boton");
         primaryStage.setScene(scene);
